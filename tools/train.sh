69797643a6d5431cb508259ba328ea6281da3a2d
zjkjzj <wy163zhuj@163.com>
2022-05-06 20:50:54 +0800

    perf(train.sh): giving more perfect tips and use

--- tools/train.sh
+++ tools/train.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-set -eux
+#set -eux
 
-# Usage: bash tools/train.sh <config-file path>
+# Usage: bash tools/train.sh <config-file> <master-port>
 
-echo "Execution file name: $0"
-echo "The first param: $1"
+if [ $# == 0 ]; then
+  echo "USAGE: CUDA_VISIBLE_DEVICES=0,1,2,3 bash tools/train.sh <config-file> <master-port>"
+  echo " e.g.1: CUDA_VISIBLE_DEVICES=0,1,2,3 bash tools/train.sh configs/cfg.yaml"
+  echo " e.g.2: CUDA_VISIBLE_DEVICES=0,1,2,3 bash tools/train.sh configs/cfg.yaml 16233"
+  exit 1
+fi
+
+master_port="15231"
+cfg_file=""
+if [ $# == 1 ]; then
+  cfg_file=$1
+elif [ $# == 2 ]; then
+  cfg_file=$1
+  master_port=$2
+fi
 
 export PYTHONPATH=.
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port="15231" \
-  tools/train/train.py -cfg $1 \
+python -m torch.distributed.launch --nproc_per_node=4 --master_port="${master_port}" \
+  tools/train.py -cfg "${cfg_file}" \
   --opt-level O1

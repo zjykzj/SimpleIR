c2679b91b1bcc427027ff8504ebe48f7606df19d
zjykzj <wy163zhuj@163.com>
2022-05-09 14:14:48 +0800

    perf(similarity): speed up calcluation

--- simpleir/metric/similarity.py
+++ simpleir/metric/similarity.py
@@ -9,16 +9,12 @@ Input query image features and search set features.
 Calculate the similarity between the query image and each image in the retrieval set
 Returns a list. The first dimension represents target and the second dimension is similarity
 """
-from typing import Dict, List, Optional
-
-import numpy as np
+from typing import Dict, List
 
 import torch
 import torch.nn.functional as F
-from sklearn.metrics.pairwise import cosine_distances, euclidean_distances
 
 
-# def euclidean_distance(query_feats: np.ndarray, gallery_feats: np.ndarray) -> np.ndarray:
 def euclidean_distance(query_feats: torch.Tensor, gallery_feats: torch.Tensor) -> torch.Tensor:
     """
     Calculate the distance between query set features and gallery set features. Derived from PyRetri
@@ -39,7 +35,6 @@ def euclidean_distance(query_feats: torch.Tensor, gallery_feats: torch.Tensor) -
     return torch.sqrt(dis)
 
 
-# def cosine_distance(query_feats: np.ndarray, gallery_feats: np.ndarray) -> np.ndarray:
 def cosine_distance(query_feats: torch.Tensor, gallery_feats: torch.Tensor) -> torch.Tensor:
     """
     Calculate the distance between query set features and gallery set features.
@@ -54,33 +49,39 @@ def cosine_distance(query_feats: torch.Tensor, gallery_feats: torch.Tensor) -> t
     similarity_matrix = F.cosine_similarity(query_feats.unsqueeze(1),
                                             gallery_feats.unsqueeze(0), dim=2)
 
-    # torch.cosine_similarity(query_feats, gallery_feats, dim=1, keepdim=True)
     return 1 - similarity_matrix
 
 
-# def similarity(feat: np.ndarray, gallery_dict: Dict, similarity_type='euclidean') \
 def similarity(feat: torch.Tensor, gallery_dict: Dict, similarity_type='euclidean') \
         -> List:
     """
     Calculate similarity (Euclidean distance / Cosine distance)
     """
     assert similarity_type in ['euclidean', 'cosine']
-    # feat_array = np.array(feat)
     if len(feat.shape) == 1:
         feat = feat.reshape(1, -1)
 
     sim_list = list()
+    key_list = list()
+    value_list = list()
+
     for idx, (key, values) in enumerate(gallery_dict.items()):
         if len(values) == 0:
             continue
 
+        key_list.extend([key for _ in range(len(values))])
+        value_list.extend(values)
+
+    if len(value_list) == 0:
+        pass
+    else:
         if similarity_type == 'euclidean':
-            tmp_sim_array = euclidean_distance(feat, torch.stack(values))[0]
+            tmp_sim_array = euclidean_distance(feat, torch.stack(value_list))[0]
         elif similarity_type == 'cosine':
-            tmp_sim_array = cosine_distance(feat, torch.stack(values))[0]
+            tmp_sim_array = cosine_distance(feat, torch.stack(value_list))[0]
         else:
             raise ValueError(f'{similarity_type} does not support')
 
-        sim_list.extend([[key, score.item()] for score in tmp_sim_array])
+        sim_list = [[key, score.item()] for key, score in zip(key_list, tmp_sim_array)]
 
     return sim_list

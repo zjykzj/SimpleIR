babd5dd1eb8357e185ef8566f45446f8ab4792d6
zjkjzj <wy163zhuj@163.com>
2022-05-11 23:03:50 +0800

    test(model): mobilenet

---
+++ tests/model/mobilenet.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2022/5/11 下午10:45
+@file: mobilenet.py
+@author: zj
+@description: 
+"""
+
+from simpleir.models.mobilenet import mobilenet_v3_large
+
+if __name__ == '__main__':
+    m = mobilenet_v3_large()
+    print(m)
+
+    state_dict = m.state_dict()
+    print(state_dict.keys())

d167eedd93664db9ae783dd8013cdcab735bc8e4
zjykzj <wy163zhuj@163.com>
2022-05-11 16:27:07 +0800

    feat(ranker): add KNN Rank

--- simpleir/metric/ranker.py
+++ simpleir/metric/ranker.py
@@ -10,15 +10,52 @@ from typing import Any, List
 
 import numpy as np
 
+from simpleir.utils.count import count_frequency
 
-def normal_rank(distance_array: np.ndarray) -> np.ndarray:
+
+def normal_rank(distance_array: np.ndarray, candidate_target_list: List, top_k: int = 10) -> List:
     if len(distance_array.shape) == 1:
         distance_array = distance_array.reshape(1, -1)
 
     # The more smaller distance, the more similar object
     sort_array = np.argsort(distance_array, axis=1)
 
-    return sort_array
+    pred_top_k_list = list()
+    for sort_arr in sort_array:
+        tmp_top_list = list(np.array(candidate_target_list)[sort_arr].astype(int))
+
+        # Returns only the sort results needed to calculate the hit rate
+        top_list = [-1 for _ in range(top_k)]
+        if len(tmp_top_list) < top_k:
+            top_list[:len(tmp_top_list)] = tmp_top_list[:]
+        else:
+            top_list = tmp_top_list[:top_k]
+
+        pred_top_k_list.append(top_list)
+    return pred_top_k_list
+
+
+def knn_rank(distance_array: np.ndarray, candidate_target_list: List, top_k: int = 10) -> List:
+    if len(distance_array.shape) == 1:
+        distance_array = distance_array.reshape(1, -1)
+
+    # The more smaller distance, the more similar object
+    sort_indexes = np.argsort(distance_array, axis=1)
+
+    sqrt_len = int(np.sqrt(len(candidate_target_list)))
+    pred_top_k_list = list()
+    for idx, sort_idx in enumerate(sort_indexes):
+        tmp_top_list = count_frequency(
+            list(np.array(candidate_target_list)[sort_idx])[:sqrt_len])
+
+        top_list = [-1 for _ in range(top_k)]
+        if len(tmp_top_list) < top_k:
+            top_list[:len(tmp_top_list)] = tmp_top_list[:]
+        else:
+            top_list = tmp_top_list[:top_k]
+
+        pred_top_k_list.append(top_list)
+    return pred_top_k_list
 
 
 def do_rank(distance_array: np.ndarray, candidate_target_list: List,
@@ -27,21 +64,10 @@ def do_rank(distance_array: np.ndarray, candidate_target_list: List,
         return None
 
     if rank_type == 'normal':
-        sort_array = normal_rank(distance_array)
+        pred_top_k_list = normal_rank(distance_array, candidate_target_list, top_k)
+    elif rank_type == 'knn':
+        pred_top_k_list = knn_rank(distance_array, candidate_target_list, top_k)
     else:
         raise ValueError(f'{rank_type} does not support')
-
-    pred_top_k_list = list()
-    for sort_arr in sort_array:
-        tmp_top_list = list(np.array(candidate_target_list)[sort_arr].astype(int))
-
-        # Returns only the sort results needed to calculate the hit rate
-        top_list = [0 for _ in range(top_k)]
-        if len(tmp_top_list) < top_k:
-            top_list[:len(tmp_top_list)] = tmp_top_list[:]
-        else:
-            top_list = tmp_top_list[:top_k]
-
-        pred_top_k_list.append(top_list)
 
     return pred_top_k_list
---
+++ simpleir/utils/count.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2022/5/11 下午3:32
+@file: count.py
+@author: zj
+@description: Refer to [python-统计元素个数](https://blog.csdn.net/u012991043/article/details/81020086#:~:text=python%E7%BB%9F%E8%AE%A1%E5%88%97%E8%A1%A8%E4%B8%AD%E5%85%83%E7%B4%A0,.count(i)%E3%80%91%E3%80%82)
+"""
+
+from typing import List
+
+import numpy as np
+
+
+def count_frequency(data_list: List[int]) -> List[int]:
+    unique_list = np.unique(data_list)
+
+    res_list = list()
+    for u in unique_list:
+        res_list.append([u, data_list.count(u)])
+
+    sorted_indices = list(reversed(np.argsort(np.array(res_list)[:, 1])))
+
+    return list(np.array(res_list)[:, 0][sorted_indices])
+
+
+def count_frequency_v2(data_list: List[int]) -> List[int]:
+    res_list = list()
+    for u in set(data_list):
+        res_list.append([u, data_list.count(u)])
+
+    sorted_indices = list(reversed(np.argsort(np.array(res_list)[:, 1])))
+
+    return list(np.array(res_list)[:, 0][sorted_indices])
+
+
+if __name__ == '__main__':
+    a = [1, 3, 1, 6, 1, 3, 1, 6, 13, 3, 5, 61, 1]
+    res_list = count_frequency(a)
+    print(res_list)
+
+    res_list = count_frequency_v2(a)
+    print(res_list)

c70132c1ac8ad163ba2c63a95114d5a1dd8156ed
zjkjzj <wy163zhuj@163.com>
2022-05-26 21:27:07 +0800

    build(python): upgrade zcls2 ~= 0.4.3 to zcls2 ~= 0.4.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ INSTALL_REQUIRES = [
     "yacs >= 0.1.8",
     "tqdm >= 4.63.0",
     "scikit_learn >= 0.24.2",
-    "zcls2 ~= 0.4.3"
+    "zcls2 ~= 0.4.4"
 ]
 CLASSIFIERS = [
     "Programming Language :: Python :: 3",

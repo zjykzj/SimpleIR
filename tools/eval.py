2ce4c00a9fa53d7ee6e78a73a34ff5672604a865
zjkjzj <wy163zhuj@163.com>
2022-05-28 11:06:12 +0800

    perf(eval.py): upgrade pretrained model use

--- tools/eval.py
+++ tools/eval.py
@@ -55,29 +55,18 @@ def init_cfg(args: Namespace) -> CfgNode:
     return cfg
 
 
-# Use a local scope to avoid dangling references
-def resume(cfg, model, device=torch.device('cpu')):
-    if os.path.isfile(cfg.RESUME):
-        logger.info("=> loading checkpoint '{}'".format(cfg.RESUME))
-        # checkpoint = torch.load(cfg.RESUME, map_location=lambda storage, loc: storage.to(device))
-        checkpoint = torch.load(cfg.RESUME, map_location=device)
-        cfg.TRAIN.START_EPOCH = checkpoint['epoch']
+def load_model(model, model_path, device=torch.device('cpu')):
+    logger.info("=> loading checkpoint '{}'".format(model_path))
+    checkpoint = torch.load(model_path, map_location=device)
 
+    if 'state_dict' in checkpoint.keys():
         state_dict = checkpoint['state_dict']
-        new_state_dict = {k.replace('module.', ''): v for k, v in state_dict.items()}
-        model.load_state_dict(new_state_dict, strict=True)
-
-        if hasattr(checkpoint, 'best_prec_list'):
-            global best_prec_list
-            best_prec_list = checkpoint['best_prec_list']
-        if hasattr(checkpoint, 'epoch'):
-            global best_epoch
-            best_epoch = checkpoint['epoch']
-
-        logger.info("=> loaded checkpoint '{}' (epoch {})"
-                    .format(cfg.RESUME, checkpoint['epoch']))
     else:
-        logger.info("=> no checkpoint found at '{}'".format(cfg.RESUME))
+        raise ValueError(f'There is no key `state_dict` in {model_path}')
+
+    state_dict = {k.replace('module.', ''): v for k, v in state_dict.items()}
+    model.load_state_dict(state_dict, strict=True)
+    logger.info("=> loaded checkpoint '{}'".format(model_path, ))
 
 
 def main():
@@ -99,7 +88,7 @@ def main():
     # Optionally resume from a checkpoint
     if cfg.RESUME:
         logger.info("=> Resume now")
-        resume(cfg, model, device=device)
+        load_model(model, cfg.RESUME, device=device)
 
     # # Data loading code
     _, _, val_loader = build_data(cfg)

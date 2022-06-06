c419fb15b338f0fd83a6c4d494eedb764825d052
zjykzj <wy163zhuj@163.com>
2022-06-06 15:32:28 +0800

    style(make_query_gallery-set): update

--- tools/eval/make_query_gallery_set.py
+++ tools/eval/make_query_gallery_set.py
@@ -4,9 +4,10 @@
 @date: 2022/4/19 上午9:59
 @file: make_query_gallery_set.py
 @author: zj
-@description: 创建查询集和图库
-1. 针对不同的数据集格式，应该拥有不一样的划分标准。比如Caltech-101、Oxford5k
-2. 对于划分过程中的随机操作，固定随机数种子，具备可复现性
+@description: Create query sets and gallery sets
+1. Specify dataset type。for example, `Caltech101` / `Oxford5k`;
+2. Specify dataset path and result path;
+3. For `General` datasets, whether to set the maximum number of saves per category.
 """
 
 import argparse
@@ -16,11 +17,14 @@ from simpleir.utils.split.build import build_splitter
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Make Query and Gallery Set")
-    parser.add_argument('-d', '--dataset', metavar='DATASET', default='Caltech101', type=str, help='Dataset type')
-    parser.add_argument('src', metavar="SRC", default='./data/caltech101/', type=str, help='Path to the src dataset')
-    parser.add_argument('dst', metavar='DST', default='./data/caltech101/', type=str, help='Path to the dst dataset')
-    parser.add_argument('-max', metavar='MAX', default=20, type=int, help="Max num each class in "
-                                                                          "dataset. Used for General")
+    parser.add_argument('-d', '--dataset', metavar='DATASET', default='Caltech101',
+                        type=str, help='Dataset type. Default Caltech101')
+    parser.add_argument('src', metavar="SRC", default='./data/caltech101/',
+                        type=str, help='Path to the src dataset')
+    parser.add_argument('dst', metavar='DST', default='./data/caltech101/',
+                        type=str, help='Path to the dst dataset')
+    parser.add_argument('-max', metavar='MAX', default=20,
+                        type=int, help="Max num each class in dataset. Just used for General")
 
     return parser.parse_args()
 

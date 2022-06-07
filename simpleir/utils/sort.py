3dd48d6af8cbfd38ebd544b44eaaea097b3cb37f
zjykzj <wy163zhuj@163.com>
2022-06-07 16:04:36 +0800

    feat(utils): add torch/numpy argsort use

---
+++ simpleir/utils/sort.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2022/6/7 上午11:57
+@file: sort.py
+@author: zj
+@description: 
+"""
+
+import torch
+
+import numpy as np
+
+
+def argsort(data: torch.Tensor) -> torch.Tensor:
+    if len(data.shape) == 1:
+        data = data.reshape(1, -1)
+
+    return torch.argsort(data, dim=1)
+
+
+def argsort_v2(data: torch.Tensor) -> torch.Tensor:
+    if len(data.shape) == 1:
+        data = data.reshape(1, -1)
+
+    return torch.from_numpy(np.argsort(data.numpy(), axis=1))
+
+
+if __name__ == '__main__':
+    torch.manual_seed(0)
+
+    data = torch.randn(3, 6)
+    print(data)
+
+    res = argsort(data)
+    print(res)
+
+    res2 = argsort_v2(data)
+    print(res)
+    assert torch.allclose(res, res2)

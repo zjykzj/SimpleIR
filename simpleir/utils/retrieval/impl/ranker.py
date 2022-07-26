b08e4b86b916eb4092f61bb0a3480aff29879ac3
zjykzj <wy163zhuj@163.com>
2022-07-26 14:45:27 +0800

    style(ranker.py): update

--- simpleir/utils/retrieval/impl/ranker.py
+++ simpleir/utils/retrieval/impl/ranker.py
@@ -44,19 +44,19 @@ def knn_rank(batch_sorts: Tensor, gallery_targets: Tensor) -> List[List]:
 def do_rank(batch_dists_tensor: Tensor, gallery_targets_tensor: Tensor,
             rank_type: RankType = RankType.NORMAL) -> Tuple[Tensor, List[List]]:
     if len(batch_dists_tensor.shape) == 1:
-        batch_dists = batch_dists_tensor.reshape(1, -1)
+        batch_dists_tensor = batch_dists_tensor.reshape(1, -1)
 
     # The more smaller distance, the more similar object
     batch_sorts = argsort(batch_dists_tensor)
 
     if rank_type is RankType.NORMAL:
-        rank_list = normal_rank(batch_sorts, gallery_targets_tensor)
+        batch_rank_label_list = normal_rank(batch_sorts, gallery_targets_tensor)
     elif rank_type is RankType.KNN:
-        rank_list = knn_rank(batch_sorts, gallery_targets_tensor)
+        batch_rank_label_list = knn_rank(batch_sorts, gallery_targets_tensor)
     else:
         raise ValueError(f'{rank_type} does not support')
 
-    return batch_sorts, rank_list
+    return batch_sorts, batch_rank_label_list
 
 
 class Ranker:
@@ -64,5 +64,5 @@ class Ranker:
     def __init__(self, rank_type: str = 'NORMAL'):
         self.rank_type = RankType[rank_type]
 
-    def run(self, batch_dists_tensor: Tensor, gallery_targets_tensor: Tensor, ):
+    def run(self, batch_dists_tensor: Tensor, gallery_targets_tensor: Tensor) -> Tuple[Tensor, List[List]]:
         return do_rank(batch_dists_tensor, gallery_targets_tensor, self.rank_type)

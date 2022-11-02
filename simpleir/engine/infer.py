cbc9a0c9aa535dc4236eca092a25f9b15a8c827d
zjykzj <wy163zhuj@163.com>
2022-11-02 14:08:07 +0800

    style(infer.py): update logger info

--- simpleir/engine/infer.py
+++ simpleir/engine/infer.py
@@ -12,6 +12,8 @@ from typing import List
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+
+import numpy as np
 from yacs.config import CfgNode
 from zcls2.util import logging
 
@@ -47,10 +49,30 @@ def validate(cfg: CfgNode, model: nn.Module, query_loader: DataLoader, gallery_l
 
     retrieval_type = cfg.RETRIEVAL.METRIC.EVAL_TYPE
     top_k_list = cfg.RETRIEVAL.METRIC.TOP_K
-    logger_str = ' * '
+    dataset = cfg.RETRIEVAL.METRIC.DATASET
     if retrieval_type == EvaluateType.MAP_OXFORD.value:
-        logger_str += f'=> MAP {top_list[0]:.3f}% '
+        logger.info(f'=> {dataset}: MAP {top_list[0]:.3f} % ')
+    elif retrieval_type == EvaluateType.MAP_ROXFORD.value:
+        if dataset.startswith('oxford5k') or dataset.startswith('paris6k'):
+            assert len(top_list) == 2
+            map, aps = top_list[:2]
+            logger.info('>> {}: mAP {:.2f}\n'.format(dataset, np.around(map * 100, decimals=2)))
+        else:
+            assert len(top_list) == 3 and len(top_list[0]) == 4
+            mapE, apsE, mprE, prsE = top_list[0]
+            mapM, apsM, mprM, prsM = top_list[1]
+            mapH, apsH, mprH, prsH = top_list[2]
+            kappas = top_k_list
+
+            logger.info('>> {}: mAP E: {}, M: {}, H: {}'.format(dataset, np.around(mapE * 100, decimals=2),
+                                                                  np.around(mapM * 100, decimals=2),
+                                                                  np.around(mapH * 100, decimals=2)))
+            logger.info(
+                '>> {}: mP@k{} E: {}, M: {}, H: {}'.format(dataset, kappas, np.around(mprE * 100, decimals=2),
+                                                             np.around(mprM * 100, decimals=2),
+                                                             np.around(mprH * 100, decimals=2)))
     else:
+        logger_str = ' * '
         for k, top in zip(top_k_list, top_list):
             if retrieval_type == EvaluateType.ACCURACY.value:
                 logger_str += f'=> Acc@{k} {top:.3f}% '
@@ -58,6 +80,6 @@ def validate(cfg: CfgNode, model: nn.Module, query_loader: DataLoader, gallery_l
                 logger_str += f'=> Prec@{k} {top:.3f}% '
             else:
                 logger_str += f'=> MAP@{k} {top:.3f}% '
-    logger.info(logger_str)
+        logger.info(logger_str)
 
     return top_list
